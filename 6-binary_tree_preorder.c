3bb89cb96354c07fa90e68cbd84e448b01ab3886
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-29 16:08:29 +0000

    4

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_tree.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_preorder - funct
+ * @tree: arg1
+ * @func: arg2
+ * Return: 0 or 1
+*/
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree == NULL || func == NULL)
+return;
+func(tree->n);
+binary_tree_preorder(tree->left, func);
+binary_tree_preorder(tree->right, func);
+}
