32568cdcaa7ba5c21abd9410d78a20c8ddaa8fb2
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-29 15:47:09 +0000

    4

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,7 +1,13 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 
-int binary_tree_is_root(const *binary_tree_t *node)
+/**
+ * binary_tree_is_root - funct
+ * @node: arg1
+ * Return: 0 or 1
+*/
+
+int binary_tree_is_root(const binary_tree_t *node)
 {
 if (node == NULL)
 {
