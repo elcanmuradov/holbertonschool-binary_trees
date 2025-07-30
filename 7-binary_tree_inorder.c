4550d940783bee3292a95b09217c9e382c929dbd
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-30 12:39:30 +0000

    3

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,14 +1,17 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 /**
- *
+ * binary_tree_inorder - func
+ * @tree - arg1
+ * @func - arg2
+ * Return: 1 or 0
 */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 if (tree == NULL || func == NULL)
 return;
-func(tree->n);
-binary_tree_preorder(tree->parent->left, func);
-binary_tree_preorder(tree->parent->right, func);
+binary_tree_preorder(tree->left, func);
+func(tree->n)
+binary_tree_preorder(tree->right, func);
 }
