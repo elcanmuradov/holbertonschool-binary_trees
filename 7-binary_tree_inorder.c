4390dde700051e05c4598c31ba17542a2dc77d10
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-30 12:33:24 +0000

    3

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -6,10 +6,9 @@
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-int temp;
 if (tree == NULL || func == NULL)
 return;
-func(tree->left);
+func(tree->n);
 tree->parent;
 binary_tree_preorder(tree->left, func);
 binary_tree_preorder(tree->right, func);
