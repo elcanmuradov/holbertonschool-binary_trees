30154ea6e793eb0099441fde3455fa1542f7ab80
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-30 13:12:14 +0000

    3

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 #include <stdlib.h>
-
+#include <stddef.h>
 /**
  * binary_tree_height - func
  * @tree: arg
@@ -9,10 +9,10 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-if (root == NULL) {
+if (tree == NULL) {
 return (-1);
 }    
-int leftHeight = height(root->left);
-int rightHeight = height(root->right);    
+int leftHeight = binary_tree_height(tree->left);
+int rightHeight = binary_tree_height(tree->right);    
 return (1 + (leftHeight > rightHeight ? leftHeight : rightHeight));
 }
