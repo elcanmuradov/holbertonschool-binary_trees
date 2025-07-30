1a6967c5c88590450f4044a6589508d3651224d5
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-30 13:22:15 +0000

    3

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,14 +9,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-int leftHeight = binary_tree_height(tree->left);
-int rightHeight = binary_tree_height(tree->right);
-if (tree->left == NULL && tree->right == NULL)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
+size_t leftHeight, rightHeight;
+if (tree == NULL)
 return (0);
-}
-if (tree == NULL) {
-return (-1);
-}    
+if (tree->left == NULL && tree->right == NULL)
+return (0);
+leftHeight = binary_tree_height(tree->left);
+rightHeight = binary_tree_height(tree->right);
 return (1 + (leftHeight > rightHeight ? leftHeight : rightHeight));
+}
 }
