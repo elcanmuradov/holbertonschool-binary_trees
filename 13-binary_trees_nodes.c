79c3f2cb1909e9707c0a8e1bdb5fc2504a22be26
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-31 14:31:51 +0000

    3

---
+++ 13-binary_trees_nodes.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_nodes - funct
+ * @tree: arg1
+ * Return: nodes
+*/
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+
+if (tree->left == NULL && tree->right == NULL)
+return (1)
+
+return (binary_tree_nodes(tree->left) + binary_tree_node(tree->right));
+}
