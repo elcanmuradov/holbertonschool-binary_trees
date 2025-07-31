c68ea83d896169b5ca63a926936aacf6536ba202
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-31 14:42:20 +0000

    3

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,8 +8,11 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-if (tree == NULL || tree->parent == NULL)
+if (tree == NULL)
 return (0);
+
+if (tree->parent == NULL && tree->left == NULL && tree->right == NULL)
+return (0)
 
 if (tree->left == NULL && tree->right == NULL)
 return (1);
